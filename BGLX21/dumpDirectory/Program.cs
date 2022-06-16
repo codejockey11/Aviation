750f4401c83b90c3d6c07112535993104981051b
codejockey11 <98759225+codejockey11@users.noreply.github.com>
2022-06-15 21:17:28 -0400

    Add files via upload

---
+++ BGLX21/dumpDirectory/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace dumpDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] filePaths = Directory.GetFiles(@"C:/Program Files (x86)/Microsoft Games/Microsoft Flight Simulator X/Scenery/", args[0], SearchOption.AllDirectories);
+            StreamWriter sw = new StreamWriter("convert.bat");
+            foreach (String fp in filePaths)
+            {
+                String f = fp.Replace('\\', '/');
+                String[] ss = f.Split('/');
+                String[] s1 = ss[ss.Length - 1].Split('.');
+                sw.Write("bglxml.exe -t -m -f \"" + f + "\" \"bgls/" + s1[s1.Length-2] + ".xml\"\r\n");
+            }
+            sw.Close();
+        }
+    }
+}
